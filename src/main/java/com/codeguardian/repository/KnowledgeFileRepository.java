5305f2efe3ebdc420f096666c46f4f21eb2fb6b4
gjg <guojiageng1024@163.com>
2026-08-05 17:36:43 +0800

    feat: add document knowledge file management

---
+++ src/main/java/com/codeguardian/repository/KnowledgeFileRepository.java
@@ -0,0 +1,1 @@
+package com.codeguardian.repository; import com.codeguardian.entity.KnowledgeFile; import org.springframework.data.jpa.repository.JpaRepository; public interface KnowledgeFileRepository extends JpaRepository<KnowledgeFile,Long>{}
